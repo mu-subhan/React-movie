2025030642eaed28705c53b461aa351bb11037d3
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-02-01 23:25:23 +0500

    search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 
+import { useState } from 'react'
 import './App.css'
+import Search from './components/Search'
 
 function App() {
-  
-
+  const [searchTerm,setSearchTerm] = useState('');
   return (
-    <>
-       <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <main>
+       <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src='./hero.png' alt="herp banner"/>
+            <h1>Find <span className='text-gradient'>Movies</span>
+            You will Enjoy without Hassle </h1>
+            </header>
+            <Search searchTerm ={searchTerm} setSearchTerm={setSearchTerm} />
+
+            <h1 className='text-white'>{searchTerm} </h1>
+        </div>
+       </div>
         
-    </>
+    </main>
   )
 }
 
